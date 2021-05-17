f23627db03c1d0ead5a795d2b85ccb686c013e35
Tymoteusz Wrzeszcz <tymoteuszwrzeszcz11@gmail.com>
2021-05-17 23:33:34 +0200

    update

--- app.py
+++ app.py
@@ -2,15 +2,18 @@ import streamlit as st
 from streamlit_webrtc import VideoProcessorBase, webrtc_streamer, WebRtcMode, ClientSettings
 import av
 import cv2
-import csv
 import numpy as np
 import pandas as pd
 import mediapipe as mp
+import tensorflow as tf
 from sklearn.pipeline import make_pipeline
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler
 from tf_bodypix.api import download_model, load_model, BodyPixModelPaths
 from PIL import Image
+from utils import visualize_boxes_and_labels_on_image_array
+import gc
+
 
 WEBRTC_CLIENT_SETTINGS = ClientSettings(
     rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
@@ -18,43 +21,63 @@ WEBRTC_CLIENT_SETTINGS = ClientSettings(
 )
 
 @st.cache(allow_output_mutation=True)
-def train(df):
-    df.dropna(axis=1, inplace=True)
+def train(saved_landmarks, landmarks):
+    df = pd.DataFrame(saved_landmarks, columns=landmarks[:len(saved_landmarks[0])])
     X = df.drop('class', axis=1)
     y = df['class']
+    del df
     pipeline = make_pipeline(StandardScaler(), RandomForestClassifier())
     pipeline.fit(X, y)
-    del df, X, y
+    del X, y
 
     return pipeline
 
 def main_section():
     st.title('Simple Computer Vision Application')
+    st.image('main_background.jpg')
+    st.markdown('This application has 3 features which utilize different machine learning models. The first app is a body '
+                'language detector where the *mediapipe* package is used to detect face, pose and hands landmarks. User can choose '
+                'one of these and capture corresponding landmarks to train a model. More details are explained in a **Body Language Decoder** '
+                'section.')
+    st.markdown('In the **Body Segmentation** section we are using the *tf_bodypix* package for segmentation. User can upload an image '
+                'which is then used as a background.')
+    st.markdown('The last feature is a **Face Mask Detector** where the object detection model trained with a *Tensorflow Object Detection API* '
+                'is used. More details about this feature can be found here [Github](https://github.com/twrzeszcz/face-mask-detection-streamlit).')
+    st.markdown('First and second feature were implemented according to the tutorials on [YouTube1](https://www.youtube.com/watch?v=We1uB79Ci-w&t=2690s) '
+                'and [YouTube2](https://www.youtube.com/watch?v=0tB6jG55mig&t=317s).')
 
 def body_language_decoder():
     train_or_predict = st.sidebar.selectbox('Select type', ['Stream and Save', 'Stream, Train and Predict'])
 
+    @st.cache(allow_output_mutation=True)
+    def get_data():
+        saved_landmarks = []
+        return saved_landmarks
+
+    saved_landmarks = get_data()
+
+    @st.cache
+    def gen_feature_names():
+        landmarks = ['class']
+        for val in range(543):
+            landmarks.extend(['x' + str(val), 'y' + str(val), 'z' + str(val), 'v' + str(val)])
+        return landmarks
+
+    landmarks = gen_feature_names()
+
     if train_or_predict == 'Stream and Save':
+        st.markdown('There are 2 types of streaming that you can choose here. It is either just the live webcam stream with displayed landmarks or a '
+                    'live stream when the selected landmarks are saved. You can choose from 4 different types of landmarks to save: *Pose and Face*, '
+                    '*Left Hand*, *Right Hand*, *Left and Right Hand*. To save landmarks you have to also specify the **class name** so the name of the '
+                    'eg. expression, gesture etc. To stop the live stream and saving just press the **Stop** button. To get the new class from the same type '
+                    'of landmarks you have to update the **class name** and start stream again. There is currently no option to have landmarks from different '
+                    'types in the same file. To use a different type you can press **Clear saved landmarks**.')
+
         stream_type = st.selectbox('Select streaming type', ['Stream only', 'Stream and save'])
-
-        @st.cache
-        def gen_feature_names():
-            landmarks = ['class']
-            for val in range(543):
-                landmarks.extend(['x' + str(val), 'y' + str(val), 'z' + str(val), 'v' + str(val)])
-            return landmarks
-
-        landmarks = gen_feature_names()
 
         model_type = st.selectbox('Select type of the model', ['Pose and Face', 'Left Hand', 'Right Hand', 'Left and Right Hand'])
 
-        if st.button('Generate New File'):
-            with open('coords.csv', mode='w', newline='') as f:
-                csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-                csv_writer.writerow(landmarks)
-
         class_name = st.text_input('Enter class name')
-
 
         class BodyDecoder(VideoProcessorBase):
             def __init__(self) -> None:
@@ -142,6 +165,7 @@ def body_language_decoder():
                         left_hand_row = list(np.array([[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in results.left_hand_landmarks.landmark]).flatten())
                         row = [self.class_name] + left_hand_row
 
+
                     elif self.model_type == 'Right Hand':
                         right_hand_row = list(np.array([[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in results.right_hand_landmarks.landmark]).flatten())
                         row = [self.class_name] + right_hand_row
@@ -151,9 +175,8 @@ def body_language_decoder():
                         right_hand_row = list(np.array([[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in results.right_hand_landmarks.landmark]).flatten())
                         row = [self.class_name] + left_hand_row + right_hand_row
 
-                    with open('coords.csv', mode='a', newline='') as f:
-                        csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-                        csv_writer.writerow(row)
+                    saved_landmarks.append(row)
+
 
                 except:
                     pass
@@ -184,15 +207,16 @@ def body_language_decoder():
             webrtc_ctx.video_processor.save = stream_type
             webrtc_ctx.video_processor.model_type = model_type
 
+
+
     if train_or_predict == 'Stream, Train and Predict':
+        st.markdown('In this section a simple machine learning model is trained on the saved landmarks. You only have to '
+                    'select the type of landmarks that were saved before.')
+
         model_type = st.selectbox('Select type of the model', ['Pose and Face', 'Left Hand', 'Right Hand', 'Left and Right Hand'])
 
-        upload_file = st.file_uploader('Choose a csv file', type='csv')
-        if upload_file is not None:
-            df = pd.read_csv(upload_file)
-            st.success('Successfully uploaded')
-            model = train(df)
-            st.success('Successfully trained')
+        model = train(saved_landmarks, landmarks)
+        st.success('Successfully trained')
 
         class BodyPredictor(VideoProcessorBase):
             def __init__(self) -> None:
@@ -316,6 +340,10 @@ def body_language_decoder():
         if webrtc_ctx.video_processor:
             webrtc_ctx.video_processor.model_type = model_type
 
+
+    if st.button('Clear saved landmarks'):
+        saved_landmarks.clear()
+
 def body_segmentation():
     img = st.file_uploader('Choose a image file', type=['jpg', 'png'])
     if img is not None:
@@ -371,10 +399,71 @@ def body_segmentation():
     if webrtc_ctx.video_processor:
         webrtc_ctx.video_processor.confidence_threshold = confidence_threshold
 
+def face_mask_detection():
+    @st.cache
+    def load_model():
+        detect_fn = tf.saved_model.load('my_model_mobnet/saved_model')
+
+        return detect_fn
+
+    detect_fn = load_model()
+
+
+    class MaskDetector(VideoProcessorBase):
+        def __init__(self) -> None:
+            self.confidence_threshold = 0.5
+            self.category_index = {1: {'id': 1, "name": 'with_mask'}, 2: {'id': 2, 'name': 'without_mask'},
+                                    3: {'id': 3, 'name': 'mask_weared_incorrect'}}
+            self.num_boxes = 1
+
+        def gen_pred(self, image):
+            input_tensor = tf.convert_to_tensor(np.expand_dims(image, axis=0))
+            detections = detect_fn(input_tensor)
+
+            num_detections = int(detections.pop('num_detections'))
+            detections = {key: value[0, :num_detections].numpy()
+                          for key, value in detections.items()}
+            detections['num_detections'] = num_detections
+            detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+
+            visualize_boxes_and_labels_on_image_array(
+                image,
+                detections['detection_boxes'],
+                detections['detection_classes'],
+                detections['detection_scores'],
+                self.category_index,
+                use_normalized_coordinates=True,
+                max_boxes_to_draw=self.num_boxes,
+                min_score_thresh=self.confidence_threshold,
+                agnostic_mode=False)
+
+            return image
+
+        def recv(self, frame: av.VideoFrame) -> av.VideoFrame:
+            image = frame.to_ndarray(format="bgr24")
+
+            image = self.gen_pred(image)
+
+            return av.VideoFrame.from_ndarray(image, format="bgr24")
 
 
 
-activities = ['Main', 'Face Mask Detection', 'Body Language Decoder', 'Body Segmentation', 'About']
+    webrtc_ctx = webrtc_streamer(
+        key="mask-detection",
+        mode=WebRtcMode.SENDRECV,
+        client_settings=WEBRTC_CLIENT_SETTINGS,
+        video_processor_factory=MaskDetector,
+        async_processing=True,
+    )
+
+    confidence_threshold = st.slider('Confidence threshold', 0.0, 1.0, 0.5, 0.05)
+    num_boxes = st.slider('Number of boxes', 1, 20, 1)
+
+    if webrtc_ctx.video_processor:
+        webrtc_ctx.video_processor.confidence_threshold = confidence_threshold
+        webrtc_ctx.video_processor.num_boxes = num_boxes
+
+activities = ['Main', 'Body Language Decoder', 'Body Segmentation', 'Face Mask Detector']
 section_type = st.sidebar.selectbox('Select Option', activities)
 
 if section_type == 'Main':
@@ -382,9 +471,15 @@ if section_type == 'Main':
 
 if section_type == 'Body Language Decoder':
     body_language_decoder()
+    gc.collect()
 
 if section_type == 'Body Segmentation':
     body_segmentation()
+    gc.collect()
+
+if section_type == 'Face Mask Detector':
+    face_mask_detection()
+    gc.collect()
 
 
 
